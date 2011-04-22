dd70bafa28cf8c9b514d4656fcd3b3f5b05159c2
Thomas Post <github@post-net.ch>
2011-04-22 16:14:52 +0200

    -added correct license/copyright header added

--- logic/tide.h
+++ logic/tide.h
@@ -1,10 +1,21 @@
 /*
- *  tide.h
- *  OpenChronos
- *
- *  Created by Thomas Post on 26.11.10.
- *
- */
+ Tide function for ez430 chronos watch.
+ 
+ Copyright (C) 2011, Thomas Post <openchronos@post-net.ch>
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef TIDE_H_
 #define TIDE_H_
